517727e8a8fdfc27c2b95260b93b8cc854333fb0
330 <330@gutmanhativa.com>
2024-11-08 12:09:41 +0200

    shalom

---
+++ app/src/main/java/com/example/recycleview/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.recycleview;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView steve;
+    ArrayList<Animals> list;
+    RecyclerCustomAdapter adapter;
+    String[] AnimalNames= {"bird", "whale"};
+    int[] AnimalPics= {R.drawable.bird, R.drawable.whale};
+    int[] AnimalRates = {4, 8};
+    String AnimalName;
+    int AnimalPic;
+    int AnimalRate;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        steve =findViewById(R.id.steve);
+        list = new ArrayList<>();
+        for (int i = 0; i < AnimalNames.length;i++)
+        {
+            list.add(new Animals(AnimalNames[i], AnimalPics[i], AnimalRates[i] ));
+
+        }
+
+        adapter = new RecyclerCustomAdapter(list);
+
+        steve.setAdapter(adapter);
+
+        steve.setLayoutManager(new GridLayoutManager(this,2));
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+
+}
